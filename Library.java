24941aa46d38c0d77359e5faaa102658a8184c90
Ewen Carr <ewencarr@hotmail.com>
2017-03-06 16:08:19 +0000

    should've commit earlier, have 5 passing tests already

---
+++ Library.java
@@ -0,0 +1,40 @@
+public class Library {
+
+  private String name;
+  private Book[] stock;
+
+  public Library(String name){
+    this.name = name;
+    this.stock = new Book[5];
+  }
+
+  public String getName() {
+    return this.name;
+  }
+
+  public int stockCount() {
+    int count = 0;
+    for (Book book : stock)
+      {
+        if (book != null) {
+          count++;
+        }
+      }
+      return count;
+  }
+
+  public void insert(Book book) {
+    int index = stockCount();
+    stock[index] = book;
+  }
+
+  // public void borrow(Book book) {
+  //   if(stockCount()) = 0 {
+  //     return;
+  //   }
+    
+  // }
+
+}
+
+
---
+++ LibraryTest.java
@@ -0,0 +1,60 @@
+import static org.junit.Assert.assertEquals;
+import org.junit.*;
+
+public class LibraryTest{
+
+  Library library;
+  Book book1;
+  Book book2;
+  Book book3;
+
+  @Before
+  public void before(){
+    library = new Library("Edinburgh Central");
+    book1 = new Book("Lord of the Rings");
+    book2 = new Book("The Wasp Factory");
+    book3 = new Book("The Old Man and the Sea");
+  } 
+
+  @Test
+  public void hasTitle(){
+    assertEquals("Lord of the Rings", book1.getTitle());
+    assertEquals("The Old Man and the Sea", book3.getTitle());
+  }
+
+// 2 assertions in one test return as one test pass
+
+  @Test
+  public void hasTitle2(){
+    assertEquals("The Wasp Factory", book2.getTitle());
+  }
+
+// and the tests need distinct names
+
+  @Test
+  public void hasName() {
+    assertEquals("Edinburgh Central", library.getName() );
+  }
+
+  @Test
+  public void stockStartsEmpty(
+    ) {
+    assertEquals(0, library.stockCount());
+  }
+
+  @Test
+  public void insertToStock() {
+    library.insert(book1);
+    assertEquals(1, library.stockCount());
+  }
+
+  // @Test
+  // public void borrowFromStock() {
+  //   library.insert(book1);
+  //   library.insert(book2);
+  //   library.borrow(book1);
+  //   assertEquals(1, library.stockCount());
+  // }
+
+
+}
